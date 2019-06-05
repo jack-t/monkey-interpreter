8ae592e1599108efb9c2d354cd2b9ea57470edd2
jack-t <jackmaverick1@gmail.com>
2019-06-05 18:41:31 -0400

    Add all the token types

--- interp.py
+++ interp.py
@@ -6,6 +6,8 @@ types = [
 	lex.text_type("return"),
 	lex.text_type("if"),
 	lex.text_type("else"),
+	lex.text_type("true"),
+	lex.text_type("false"),
 	lex.TokenType("integer-literal", r'^([0-9]+)\b', 1, lex.TokenPayloadType.INT),
 	lex.TokenType("string-literal", r'^([\"\'])(?P<payload>.*?)(\1)', "payload", lex.TokenPayloadType.STRING),
 	lex.TokenType("identifier", r'^(_*?[a-zA-Z]\w*)\b', 1, lex.TokenPayloadType.STRING),
@@ -16,7 +18,9 @@ types = [
 	lex.TokenType("star", r'^\*'),
 	lex.TokenType("slash", r'^\/'),
 	lex.TokenType("open-brace", r'^\{'),
-	lex.TokenType("close-brace", r'^\{'),
+	lex.TokenType("close-brace", r'^\}'),
+	lex.TokenType("open-bracket", r'^\['),
+	lex.TokenType("close-bracket", r'^\]'),
 	lex.TokenType("open-paren", r'^\('),
 	lex.TokenType("close-paren", r'^\)'),
 	lex.TokenType("comma", r'^\,'),
@@ -25,5 +29,5 @@ types = [
 
 lexer = lex.Lexer(types)
 
-for tok in lexer.tokenize(" letx \"def\" if  'abc' fn return abc 1 ="):
+for tok in lexer.tokenize(" let fn return if else 1 \"def\" 'abc' _Abc = == + - * / {}()[],;"):
 	print(tok)
